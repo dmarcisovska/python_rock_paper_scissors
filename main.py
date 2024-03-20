15aaa7af30d3d253d58639c43c349223c8daabb0
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-19 19:29:29 -0700

    added conditional statements and testing logic

--- main.py
+++ main.py
@@ -18,4 +18,25 @@ elif ran_num == 1:
 elif ran_num == 1:
     player_one = "scissors"
 
+print(player_one)
+
+if player_one == player_two:
+    print("it's a tie!")
+elif player_one == "rock" and player_two == "scissors":
+    print("Player one wins")
+elif player_one == "scissors" and player_two == "paper":
+    print("Player one wins")
+elif player_one == "paper" and player_two == "rock":
+    print("Player one wins")
+elif player_two == "rock" and player_one == "scissors":
+    print("Player one wins")
+elif player_two == "scissors" and player_one == "paper":
+    print("Player one wins")
+elif player_two == "paper" and player_one == "rock":
+    print("Player one wins")
+else:
+    print("something went wrong")
+
+
+
 
