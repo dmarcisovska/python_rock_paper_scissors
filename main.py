6572288c482b76cc1c45a1e515e28dca6c7662f0
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-19 19:26:25 -0700

    set player one to rock paper or scissors based on random int

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ print("Scissors...")
 ran_num = random.randint(1, 3)
 
 player_one = None
-# player_two = input("Make your move: ")
+player_two = input("Make your move: ")
 
 print(ran_num)
+
+if ran_num == 1:
+    player_one = "rock"
+elif ran_num == 1:
+    player_one = "paper"
+elif ran_num == 1:
+    player_one = "scissors"
+
 
